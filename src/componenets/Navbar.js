af63208cb9f1667afacb8978bf4d017e64ef4210
Gunwant35 <gunwant.siddhu35@gmail.com>
2025-01-23 23:27:06 +0530

    Initial skeleton

---
+++ src/componenets/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div
+      style={{
+        border: "2px solid black",
+        display:"flex",
+        justifyContent:"center"
+      }}
+    >
+      <h2>Key Bored</h2>
+    </div>
+  );
+}
+
+export default Navbar;
